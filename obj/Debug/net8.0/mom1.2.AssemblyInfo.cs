d7a39af5af6dd3aac97aa009607076a8a02affed
Cutecowboy <110782320+Cutecowboy@users.noreply.github.com>
2024-02-01 13:22:01 +0100

    added component 'about' which can be toggled on/off via button

--- obj/Debug/net8.0/mom1.2.AssemblyInfo.cs
+++ obj/Debug/net8.0/mom1.2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mom1.2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a424ae1c5c5069c29d2591682048485b90342cfd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("mom1.2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mom1.2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
